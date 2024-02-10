2ab84aed07f124a951fec38c26e94143a1428510
Jean Demeusy <jdemeusy@me.com>
2024-02-10 22:22:57 +0100

    Initial Commit

---
+++ NodeAdmin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NodeAdmin
+//
+//  Created by Jean Demeusy on 2/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NodeAdmin/NodeAdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  NodeAdminApp.swift
+//  NodeAdmin
+//
+//  Created by Jean Demeusy on 2/10/24.
+//
+
+import SwiftUI
+
+@main
+struct NodeAdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
